04c89c62e6c39953850bce7a6ac70f6fd6cc4fae
genjix2 <96345719+genjix2@users.noreply.github.com>
2022-02-10 15:56:30 +0700

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#signin-link').click(function(e) {
+		$("#signin").delay(100).fadeIn(100);
+ 		$("#signup").fadeOut(100);
+		$('#signup-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#signup-link').click(function(e) {
+		$("#signup").delay(100).fadeIn(100);
+ 		$("#signin").fadeOut(100);
+		$('#signin-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
